c20c8440839752f221141e3ef69ca5389da92b16
Almujtaba <mujtabaimad8@gmail.com>
2024-05-01 21:00:36 +0400

    feat: added saving universities list in localStorage

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -3,31 +3,29 @@ import { fetchUniversities } from "../../services/universities";
 import { UniversityType } from "../../models/university-model";
 import UniversityListItem from "../../components/list-item/list-item";
 
-import "./home.scss"
+import "./home.scss";
 
 function Home() {
   const [universities, setUniversities] = useState<UniversityType[]>();
 
   useEffect(() => {
     // fetching universities list on first load
-    fetchUniversities()
-      .then((universitieslist) => {
-        setUniversities(universitieslist);
-      })
-      .catch(() => {
-        // TODO chech localStorage if the api fetch faild
-      });
+    fetchUniversities().then((universitieslist) => {
+      setUniversities(universitieslist);
+    });
   }, []);
 
   const universitiesList = useMemo(() => {
     return universities?.map((university) => {
-      return <UniversityListItem university={university} key={university.name}/>;
+      return (
+        <UniversityListItem university={university} key={university.name} />
+      );
     });
   }, [universities]);
 
   return (
     <div className="home-wrapper">
-      <div className="page-title">Universities List</div>
+      <div className="page-title">UAE Universities List</div>
       <div className="universities-list">{universitiesList}</div>
     </div>
   );
--- src/services/universities.ts
+++ src/services/universities.ts
@@ -2,12 +2,31 @@ import axios from "axios"
 import { UniversityType } from "../models/university-model"
 
 export const UNIVERSITIES_API_URL = "http://universities.hipolabs.com/search?country=United%20Arab%20Emirates"
+const UNIVERSITIES_LOCALSTORAGE_KEY = "universities"
+const DEBUG_MODE_ENABLED = true;
 
+// fetch universities list and save it in local storage and handle if an error occurred
+export async function fetchUniversities() {
+    return axios.get<UniversityType[]>(UNIVERSITIES_API_URL).then((res) => {
 
-
-export function fetchUniversities(){
-    return axios.get<UniversityType[]>(UNIVERSITIES_API_URL).then((res)=>{
         const data = res.data;
+        localStorage.setItem(UNIVERSITIES_LOCALSTORAGE_KEY, JSON.stringify(data))
         return data;
+
+    }).catch((error) => {
+
+        // incase off error while fetching universities: log the error 
+        // and check if universities list is available in local storage and if so return it instad
+        if (DEBUG_MODE_ENABLED) {
+            console.error(error)
+        }
+
+        const localStorageUniversities = localStorage.getItem(UNIVERSITIES_LOCALSTORAGE_KEY);
+        if (localStorageUniversities) {
+            const parsedUniversities:UniversityType[] = JSON.parse(localStorageUniversities);
+            return parsedUniversities;
+        } else {
+            return []
+        }
     })
 }
